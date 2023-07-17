1cfa20963828fc405574e6753307cd342b23efc9
Mari <dennismaricarl@gmail.com>
2023-07-17 15:15:05 -0700

    added vault files

---
+++ vault.js
@@ -0,0 +1,17 @@
+/*pseudocode:
+Steps:
+1. Create a dialog box with a message alerting that the user has been chose to open the vault with a combination code.
+2. Create 3 variables and assign each with an arithmetic calculation resulting in the first vault code number, then the second, then the third.
+3. Create an alert displaying the vault code combination. The combination is 10 - 40- 39.
+*/
+
+//Step 1:
+alert ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+//Step 2:
+let codeOne = 100 / 10;
+let codeTwo = 10 + 30;
+let codeThree = 40 - 1;
+
+//Step 3:
+alert("Your vault code is " + codeOne + "-" + codeTwo + "-" + codeThree)
